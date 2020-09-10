47db5a983385e92ddb9e46bc5028c3d813857532
olme marin <olme_gms@hotmail.com>
2020-09-09 22:29:09 -0500

    Login y registro terminado

---
+++ lib/widgets/terminos.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Terminos extends StatelessWidget {
+  const Terminos({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Términos y condiciones de uso',
+      style: TextStyle(fontWeight: FontWeight.w200),
+    );
+  }
+}
